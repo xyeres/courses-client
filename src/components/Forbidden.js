2e354f465c133512e48cc8fcdce1cac1be554679
Michael Carr <xyeres@me.com>
2021-09-13 13:01:01 -0700

    Add svgs

--- src/components/Forbidden.js
+++ src/components/Forbidden.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import FourOhFourImage from '../img/courses/svgs/hacker-60b6.svg'
+
 /**
   * Simple user message if they are not allowed to access a route
 */
 export default function Forbidden() {
     return (
-        <div className="container">
+        <div className="container flex-center">
+            <img width="450" height="250" src={FourOhFourImage} alt="404 error toilet paper roll" />
             <h1>You're not authorized to do that.</h1>
         </div>
     )
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import FourOhFourImage from '../img/courses/svgs/404-60b6.svg'
 
 /**
   * Simple global 404 error 
@@ -8,7 +8,9 @@ import { Link } from 'react-router-dom'
 export default function NotFound() {
     return (
         <div className="container">
+            <img width="450" height="250" src={FourOhFourImage} alt="404 error toilet paper roll" />
             <h1>Whoops! 404 - The requested item does not exist.</h1>
+            <br />
             <Link to="/" className="btn form__btn">Back home</Link>
         </div>
     )
